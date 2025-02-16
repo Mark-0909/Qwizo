864d098a66dd3b2cda0eb98f12f12eda660e89f6
Mark Orcullo <orcullomark856@gmail.com>
2025-02-16 15:28:57 +0800

    added some design

--- src/app/components/teacherPage/quizzes.tsx
+++ src/app/components/teacherPage/quizzes.tsx
@@ -27,18 +27,18 @@ const Quizzes: React.FC<QuizzesProps> = ({ activePage, onChangePage }) => {
   return (
     <div>
       <div className="flex items-center justify-between w-full">
-        <h1>Quizzes</h1>
+        <h1 className="text-3xl font-bold">Quizzes</h1>
 
         <button
           key="CREATEQUIZ"
           onClick={() => onChangePage("CREATEQUIZ")}
-          className="bg-blue-900 p-3 text-white"
+          className="bg-blue-900 p-3 text-white rounded"
         >
           Add quiz
         </button>
       </div>
       <div>
-        <hr className="m-5" />
+        <hr className="m-5 border-gray-300" />
       </div>
     </div>
   );
